ce38152bce1c32b3712310f4256825d7b5c007a3
Boss Udomsuktham <bossiccs@Bosss-MacBook-Pro.local>
2022-05-14 22:04:56 +0700

    optimizing image data

--- controller/posts.js
+++ controller/posts.js
@@ -1,13 +1,30 @@
 const Post = require("../models/Post");
 const { StatusCodes } = require("http-status-codes");
+const axios = require("axios");
 
 const getAllPosts = async (req, res) => {
-  const posts = await Post.find({}).sort("-timestamp");
+  const posts = await Post.find().sort("-timestamp");
   res.status(StatusCodes.OK).json({ posts });
 };
 const createPost = async (req, res) => {
-  const post = await Post.create(req.body);
-  res.status(StatusCodes.CREATED).json({ post });
+  // console.log(req.body);
+  const { postImage, ...rest } = req.body;
+  // console.log(rest);
+  try {
+    const data = new FormData();
+    data.append("file", postImage);
+    data.append("upload_preset", "default-preset");
+    const uploadImage = await axios.post(
+      "https://api.cloudinary.com/v1_1/doy4bvcru/image/upload",
+      data
+    );
+
+    console.log(uploadImage.data.url);
+  } catch (error) {
+    console.log(error);
+  }
+  // const post = await Post.create(req.body);
+  res.status(StatusCodes.CREATED).json({ post: 1 });
 };
 
 module.exports = {
--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,5 @@ const router = express.Router();
 
 const { getAllPosts, createPost } = require("../controller/posts");
 router.route("/").get(getAllPosts).post(createPost);
-// router.route('/:id')
 
 module.exports = router;
